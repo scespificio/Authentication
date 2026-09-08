4944cdb1ee0d36e564c3b47b3b9793886d27c3bb
romainferigo-sudo <romain.ferigo@espificio.com>
2026-09-08 09:11:18 +0000

    Remove bash files

--- reset.sh
+++
@@ -1,4 +0,0 @@
-
-sudo docker compose -f /home/adminroot/services/run/Authenticate_dc.dev.yml -p authenticate down
-sudo docker compose -f /home/adminroot/services/run/Authenticate_dc.dev.yml -p authenticate up --build
-sudo docker system prune -f
